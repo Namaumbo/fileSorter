cd8e05131c01a74595890fefc050dd7156a5b0bc
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2023-08-26 23:40:44 +0200

    rf

--- fileSorter.py
+++ fileSorter.py
@@ -16,10 +16,20 @@ current_dir = os.getcwd()
 videos_ext = ['.mp4', '.mkv', '.avi','.webm','.3gp']
 music_ext = ['.mp3', '.wav' ,'.midi']
 pictures_ext =['.jpg', '.jpeg', '.gif', '.png','.svg']
+docs_ext = ['.pdf', '.docx']
 
 videos_path = os.path.join(os.getcwd(), 'videos')
 music_path = os.path.join(os.getcwd(), 'music')
 pictures_path = os.path.join(os.getcwd(), 'pictures')
+docs_path = os.path.join(os.getcwd(), 'docs')
+
+
+def path_filter(full_path , file):
+    full_path = os.path.join(os.getcwd(),full_path)
+    file_path = pathlib.PurePath(os.path.join(os.getcwd(), file))
+
+    return full_path, file_path
+
 
 for file in os.listdir(current_dir):
     print(file)
@@ -29,10 +39,9 @@ for file in os.listdir(current_dir):
             os.mkdir(videos_path)
         else :
             try:
-                full_path = os.path.join(os.getcwd(),videos_path)
-                file_path = pathlib.PurePath(os.path.join(os.getcwd(), file))
+                full_path , file_path = path_filter(videos_path,file)
                 shutil.move(file_path,full_path)
-            except OSError as e:
+            except Exception as e:
                   print ('Failed to move video to %s: %s' % (videos_path, e))
 
     elif file_ext in music_ext:
@@ -41,10 +50,9 @@ for file in os.listdir(current_dir):
 
         else:     
             try:
-                full_path = os.path.join(os.getcwd(),music_path )
-                file_path = pathlib.PurePath(os.path.join(os.getcwd(), file))
+                full_path , file_path = path_filter(music_path,file)
                 shutil.move(file_path, full_path)  
-            except OSError as e:
+            except Exception as e:
                 print ('Failed to move music to %s: %s' % (music_path, e))
 
     elif file_ext in pictures_ext:   
@@ -53,11 +61,22 @@ for file in os.listdir(current_dir):
             os.mkdir(pictures_path)
         else:
             try:
-                full_path = os.path.join(os.getcwd(),pictures_path )
-                file_path = pathlib.PurePath(os.path.join(os.getcwd(), file))
+                full_path , file_path = path_filter(pictures_path ,file)
                 shutil.move(file_path,full_path)
-            except OSError as e:
+            except Exception as e:
                 print ('Failed to move picture to %s: %s' % (videos_path, e))
+    
+    elif file_ext in docs_ext:   
+        if not os.path.exists(docs_path):
+            os.mkdir(docs_path)
+        else:
+            try:
+            
+                full_path , file_path =path_filter(docs_path , file)
+                shutil.move(file_path,full_path)
+            except Exception as e:
+                print ('Failed to move picture to %s: %s' % (docs_path, e))
+
 
     else:
         continue
